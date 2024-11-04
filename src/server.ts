96f356fc46e126fbadebb8e05b441346a9cf9701
Dragomir Denev <ddenev@gmail.com>
2024-11-04 23:08:46 +0200

    added enterpriseVersion option

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { start } from './lib/server-impl';
 
 try {
-    start();
+    start({
+        enterpriseVersion: '6.4.0'
+    });
 } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
